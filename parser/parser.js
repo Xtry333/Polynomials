6ed806c75549623e188eedaa81a37db634690503
Xtry333 <xtry333@gmail.com>
2018-11-13 21:58:26 +0100

    parser

--- parser/parser.js
+++ parser/parser.js
@@ -2,9 +2,25 @@ $(function() {
     function parse() {
         var lines = textIn.value.replace("\r", "").split("\n");
 
-        
+        let out = [];
 
-        var s = text.replace(/\[S\]/gm, "<blockquote><p>").replace(/\[E\]/gm, "</p></blockquote>");
+        var indent = 0;
+        for (let i = 0; i < lines.length; i++) {
+            let e = lines[i];
+            while (true) {
+                if (e[indent] == ">") {
+                    indent++;
+                } else {
+                    break;
+                }
+            }
+            
+            console.log(indent);
+        }
+
+        //console.log(indent);
+
+        var s = out.join("\n").replace(/\[S\]/gm, "<blockquote><p>").replace(/\[E\]/gm, "</p></blockquote>");
         textOut.value = s;
         divOut.innerHTML = s;
     };
